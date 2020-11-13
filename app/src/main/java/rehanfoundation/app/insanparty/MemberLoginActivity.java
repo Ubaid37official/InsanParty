532119ae303fe2ba02737fcb1859effad8ecb280
Ubaid Ur Rehman <s.u.shah68@gmail.com>
2020-11-13 20:39:46 +0500

    new activity added

--- app/src/main/java/rehanfoundation/app/insanparty/MemberLoginActivity.java
+++ app/src/main/java/rehanfoundation/app/insanparty/MemberLoginActivity.java
@@ -1,16 +1,40 @@
 package rehanfoundation.app.insanparty;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class MemberLoginActivity extends Activity {
 
+    private EditText email, password;
+    private  TextView forgotPassword;
+    private Button login;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memberlogin);
+
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        forgotPassword = (TextView)findViewById(R.id.forgotPassword);
+
+        login = (Button)findViewById(R.id.login);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),UserHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/rehanfoundation/app/insanparty/UserLoginActivity.java
+++ app/src/main/java/rehanfoundation/app/insanparty/UserLoginActivity.java
@@ -1,16 +1,39 @@
 package rehanfoundation.app.insanparty;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class UserLoginActivity extends Activity {
 
-
+    private Button login;
+    private EditText email, password;
+    private TextView forgotPassword;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlogin);
+
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        forgotPassword = (TextView)findViewById(R.id.forgotPassword);
+
+        login = (Button)findViewById(R.id.login);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),UserHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
